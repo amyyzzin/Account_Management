47e45f0708afab4e38a765c564d4e284b244f1e9
DESKTOP-BNVQCL4\82109 <amyyzzin@naver.com>
2022-07-27 14:18:19 +0900

    Zerobase_Project_02

---
+++ src/main/resources/data.sql
@@ -0,0 +1,7 @@
+/*insert into accoount_user(id, name, created_at, updated_at)
+values (1, 'yumeming', now(), now())
+insert into accoount_user(id, name, created_at, updated_at)
+values (2, 'mingtori', now(), now())
+insert into accoount_user(id, name, created_at, updated_at)
+values (3, 'zingtori', now(), now())
+*/
